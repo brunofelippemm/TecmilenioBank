b7a30d9febb56580ec41fd2059e6ebea0436d885
itsMarlon <mdanielcarvallo@gmail.com>
2023-03-13 11:08:31 -0600

    cambios realizados en el código, para le eficiencia

--- user.py
+++ user.py
@@ -1,29 +1,21 @@
 class User:
-    credentials = dict()
+    credentials = {'':''}
     def register(self):
         print("-----Bienvenido al Banco Tecmilenio-----")
 
         print("Ingresa su correo")
         email = input()
-        if (email==User.credentials):
+       
+        if (User.credentials.get(email,False)):
             print("Este correo ya está en uso")
         else:
             print("ingrese su contraseña")
             password=input()
-            if(password==User.credentials):
-                print("Esta contraseña ya está en uso")
-            else:
+            if(User.credentials.get(email,False)):
                 print("Su cuenta se ha creado correctamente")
-            User.credentials[email] = password
-        #    if (User.credentials[email]==password):
-         #       print("esta contraseña ya está en uso")
-          #  else:
-           #     print("Confirme su contraseña")
-            #    confirmpassword = input()
-            
-               
-             ##      print("Se ha creado correctamente la cuenta")
-            print(User.credentials)
+                User.credentials[email] = password
+        
+
 pepe = User()
-pepe.register()     
+pepe.register() 
 #add commit push -u origin 
